7b9c7f647dcf158a38b4ddba79a825196cce2525
Abdur Rahman <ar7165@nyu.edu>
2024-12-08 08:40:48 -0500

    docking

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || '/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://lazydrobe-23933613008.us-central1.run.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
